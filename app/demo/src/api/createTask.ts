a55467b75fe16db1649838a437948f56098eab08
mavvystudio <mavvystudio@gmail.com>
2025-09-23 04:50:28 +0800

    add gh workflow to publish to npm

--- app/demo/src/api/createTask.ts
+++ app/demo/src/api/createTask.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { prisma } from "../lib/db";
+import type { Task } from "@prisma/client";
 
 export const input = z.object({
   title: z.string(),
@@ -7,27 +8,29 @@ export const input = z.object({
   status: z.enum(['active', 'inactive']).default('active'),
 });
 
-export const roles = ['ADMIN'];
+export const roles = ['ADMIN', 'MEMBER'];
 
 export const routePath = '/tasks';
 
 export const method = 'POST';
 
 export const response = z.object({
-  task: z.object({
-    id: z.string(),
-    title: z.string(),
-    description: z.string(),
-    status: z.enum(['active', 'inactive']),
-  }),
+  id: z.string(),
+  title: z.string(),
+  description: z.string(),
+  status: z.string(),
+  userId: z.string().nullable(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
 });
+
 
 export const handler = createTask;
 
 async function createTask(params: {
   input: z.infer<typeof input>,
   authData: { id: string, role: string, name: string, email: string }
-}) {
+}): Promise<z.infer<typeof response>> {
   const { title, description, status } = params.input;
   console.log('authData', params.authData);
 
@@ -35,5 +38,5 @@ async function createTask(params: {
     data: { title, description, status },
   });
 
-  return { task };
+  return task;
 }
